eb1f1d8cd64066d8ff255f263793773f21fc366a
Esraa-MOhamed7 Mohamed <israa.m7hamed0@gmail.com>
2025-08-10 07:43:51 +0000

    completed project

--- RPS.py
+++ RPS.py
@@ -1,10 +1,26 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+from collections import Counter
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+def player(prev_play, opponent_history=[], my_history=[]):
+    beaters = {"R": "P", "P": "S", "S": "R"}
+    prediction_score = {"R": 0, "P": 0, "S": 0}
+
+    if prev_play:
+        opponent_history.append(prev_play)
 
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    pattern_length = 4
+    if len(opponent_history) > pattern_length:
+        last_pattern = "".join(opponent_history[-pattern_length:])
+        possible_next_moves = []
+        for i in range(len(opponent_history) - pattern_length):
+            if "".join(opponent_history[i:i + pattern_length]) == last_pattern:
+                possible_next_moves.append(opponent_history[i + pattern_length])
+        if possible_next_moves:
+            most_common_next = Counter(possible_next_moves).most_common(1)[0][0]
+            guess = beaters[most_common_next]
+        else:
+            most_common_overall = Counter(opponent_history).most_common(1)[0][0]
+            guess = beaters[most_common_overall]
 
+    my_history.append(guess)
     return guess
